e048ff0a10667d9e3b6e411a0ffce8de8c7cb5f5
chuonglam209 <14520090@gm.uit.edu.vn>
2021-10-24 22:44:04 +0700

    index.html

---
+++ Section 8/Ex.js
@@ -0,0 +1,69 @@
+// BT1
+    // const container = document.getElementById('list');
+    // for(let i = 0; i < 3; i++) {
+    //   container.insertAdjacentHTML('beforeend', `<li>${i}</li>`)
+    // }
+    //result
+    // 1.d
+    // 2.a
+    // 3.c
+    //4.b
+//BT2
+    // setInterval(function(){
+    //     alert("I'm Iron Man"); }, 4000);
+
+
+
+
+
+
+// BT4
+ 
+    // function myFunction() {
+    //     let x = document.getElementsByTagName("LI");
+    //     document.getElementById("demo").innerHTML = x[0].innerHTML;
+    //     for(let i = 0 ;i < 3 ; i++ )
+    //     {
+    //         x[i].innerHTML;
+    //     console.log(x[i].innerHTML);
+    //     }
+    //   }
+
+
+//BT5
+    // function myFunction5() {
+    //     let x = document.getElementsByClassName("singer")
+    //     document.getElementById("demo").innerHTML = x[2].innerHTML;
+    //     for(let i = 0 ;i < 4 ; i++ )
+    //     {
+    //         x[i].innerHTML;
+    //     console.log(x[i].innerHTML);
+    //     }
+    //   }
+  
+
+// BT8
+    // function myName()
+    // {
+    //     alert("my name's Chuong");
+    //     alert("one wish of the user this year: change job");
+    // }
+    // myName();
+
+ 
+
+// BT9
+    // function hi(name,oneWish){
+    //     alert(`my name's ${name}`);
+    //     alert(`one wish of the user this year: ${oneWish} `);
+    // }
+    // hi('chuong','change job');
+
+
+// BT10
+//     function hi(name,oneWish){
+//         alert(`my name's ${name}`);
+//         if (!oneWish) alert("missing onWish")
+//         alert(`one wish of the user this year: ${oneWish} `);
+//         }
+//     hi('chuong');
